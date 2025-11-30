6e62f9b41d98780dd722e28c8130ccb4aada4587
Overbusher <kirill.raev.90@mail.ru>
2025-11-30 21:31:23 +0300

    Added Alt attribute for img

--- src/body/main_body/home/Home.jsx
+++ src/body/main_body/home/Home.jsx
@@ -2,7 +2,7 @@ import './Home.css'
 
 const Home = () => {
     return (<div>
-            <div className='home'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Stars_01_%28MK%29.jpg/960px-Stars_01_%28MK%29.jpg'/></div>
+            <div className='home'><img alt="Фон профиля" src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Stars_01_%28MK%29.jpg/960px-Stars_01_%28MK%29.jpg'/></div>
             <div>Ava</div>
             <div>Имя и Фамилия</div>
             <div>Посты:</div>
