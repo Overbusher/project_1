2185270f89b512047a39a57cd35e08e05df44cfe
Overbusher <kirill.raev.90@mail.ru>
2025-12-06 22:33:14 +0300

    Messages.jsx and Messages.module.css were filled with data.

--- src/body/main_body/messages/Messages.jsx
+++ src/body/main_body/messages/Messages.jsx
@@ -2,6 +2,18 @@ import st from './Messages.module.css'
 
 export const Messages = () => {
     return (
-        <div className={st.msg}>Messages</div>
+        <div className={st.msg}>
+        <div className={st.dialogs}>
+            <div className={st.active}>Diana</div>
+            <div>Timofey</div>
+            <div>Igor</div>
+            <div>Vanya</div>
+        </div>
+        <div className={st.messages}>
+            <div>Hi!!</div>
+            <div>How are you?</div>
+            <div>You are here?</div>
+        </div>
+        </div>
     )
 }
