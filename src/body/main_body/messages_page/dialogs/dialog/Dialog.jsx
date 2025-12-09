0e7932438f039b0539b098d864ff9a9d3aa4745a
Overbusher <kirill.raev.90@mail.ru>
2025-12-09 22:42:54 +0300

    Dialog.jsx and Dialog.module.css have been added to the project and filled in.

--- src/body/main_body/messages_page/dialogs/dialog/Dialog.jsx
+++ src/body/main_body/messages_page/dialogs/dialog/Dialog.jsx
@@ -1,9 +1,11 @@
-import st from './Dialogs.module.css'
+import st from "./Dialog.module.css"
 import {NavLink} from "react-router-dom";
 
-export const Dialogs = (props) => {
+export const Dialog = (props) => {
     let path = "/messages/" + props.id;
+
     let activated = ({isActive}) => isActive ? st.active : st.dialogs;
+
     return (
         <div className={st.dialogs}>
             <NavLink to={path} className={activated}>{props.name}</NavLink>
