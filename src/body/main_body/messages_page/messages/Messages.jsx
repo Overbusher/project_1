6d75ff987696b6b016ef12c8bd3f1f75bb544b2a
Overbusher <kirill.raev.90@mail.ru>
2025-12-11 23:01:21 +0300

    Changed the state.js

--- src/body/main_body/messages_page/messages/Messages.jsx
+++ src/body/main_body/messages_page/messages/Messages.jsx
@@ -3,7 +3,11 @@ import {Message} from "./message/Message";
 
 
 export const Messages = (props) => {
-
+    // const location = useLocation();
+    // const path = location.pathname;
+    // const lastChar = +path.slice(-1);
+    // let msg = props.messageData.find(ms => ms.id === lastChar);
+    // console.log(msg);
     let messageData = props.messageData.map(msg =>
         <Message key={msg.id} id={msg.id}
                 message={msg.message}/>)
@@ -11,6 +15,7 @@ export const Messages = (props) => {
     return (
         <div className={st.messages}>
             {messageData}
+            {/*<Message message={props.messageData}/>*/}
         </div>
     )
 }
--- src/body/main_body/messages_page/messages/message/Message.jsx
+++ src/body/main_body/messages_page/messages/message/Message.jsx
@@ -1,12 +1,15 @@
 import st from './Message.module.css'
+// import {useLocation} from "react-router-dom";
 
 export const Message = (props) => {
     // const location = useLocation();
-    // const ident ="/message/" + ' ' + props.id;
-    // console.log(ident);
+    // const path = location.pathname;
+    // const lastChar = +path.slice(-1);
+    // let msg = (props.id === lastChar) ? props.message : null;
+    // console.log(msg);
     return (
         <div className={st.message}>
-            <li>{props.message}</li>
+            {props.message}
         </div>
     )
 }
