b92aa64479fa557c96f619c1369e831d05960ea0
Overbusher <kirill.raev.90@mail.ru>
2025-12-19 01:48:25 +0300

    state.js has been renamed to store.js A Redux module has been added to the project. A reduxStore.js has been added to the project. Data from store.js(state.js) have been refactored and moved to reduxStore.js.

--- src/redux/messagesReducer.js
+++ src/redux/messagesReducer.js
@@ -1,7 +1,38 @@
 let ADD_MESSAGE = 'ADD_MESSAGE';
 const INPUT_MESSAGE = 'INPUT_MESSAGE';
 
-const messagesReducer = (state, action) => {
+let initialState = {
+    dialogData: [
+        {key: 1, id: 1, name: 'Diana'},
+        {key: 2, id: 2, name: 'Vitaliy'},
+        {key: 3, id: 3, name: 'Olga'},
+        {key: 4, id: 4, name: 'Vera'},
+        {key: 5, id: 5, name: 'Timofei'},
+        {key: 6, id: 6, name: 'Vanya'}
+    ],
+    messageData: [
+        {key: 1, id: 1, message: 'Привет'},
+        {key: 2, id: 2, message: 'Привет'},
+        {key: 3, id: 2, message: 'Вы завтра к нам?'},
+        {key: 4, id: 1, message: 'Я тебя очень сильно люблю!'},
+        {key: 5, id: 1, message: 'Как твои дела?'},
+        {key: 6, id: 1, message: 'Не грусти из за того что мы поругались, мы помиримся и всё будет хорошо'},
+        {key: 7, id: 2, message: 'Я буду готовить курицу'},
+        {key: 8, id: 3, message: 'Привет'},
+        {key: 9, id: 4, message: 'Vera'},
+        {key: 10, id: 3, message: 'Я отдала весы'},
+        {key: 11, id: 3, message: 'Как у вас дела?'},
+        {key: 12, id: 4, message: 'Ха-ха'},
+        {key: 13, id: 4, message: 'а я пишу код лучше чем ты'},
+        {key: 14, id: 5, message: 'Привет, играть будешь?'},
+        {key: 15, id: 5, message: 'Ты тут?'},
+        {key: 16, id: 6, message: 'Привет, я сегодня не буду'},
+        {key: 17, id: 6, message: 'Чувствую себя не оч'}
+    ],
+    inputMessageData: ''
+}
+
+const messagesReducer = (state = initialState, action) => {
 
     switch (action.type) {
         case ADD_MESSAGE:
--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -1,7 +1,33 @@
 let ADD_POST = 'ADD_POST';
 let INPUT_POST = 'INPUT_POST';
 
-const profileReducer = (state, action) => {
+let initialState = {
+    postData: [
+        {
+            id: 1,
+            text: 'FUS RO DAH!',
+            src: 'https://sun9-55.userapi.com/s/v1/ig2/ZyBsxGr_o07pRdrAAniwTaTTNbXy4UIHSQffk5IdOGzeZV_Dr1byiJ3_m2zzdHKjnqNmRu53iphy4avSqZJiWJrM.jpg?quality=96&as=32x32,48x48,72x72,108x108,160x161,228x229&from=bu&cs=228x0'
+        },
+        {
+            id: 2,
+            text: 'LIGHT WEIGHT, BABY!!!',
+            src: 'https://sun9-55.userapi.com/s/v1/ig2/ZyBsxGr_o07pRdrAAniwTaTTNbXy4UIHSQffk5IdOGzeZV_Dr1byiJ3_m2zzdHKjnqNmRu53iphy4avSqZJiWJrM.jpg?quality=96&as=32x32,48x48,72x72,108x108,160x161,228x229&from=bu&cs=228x0'
+        },
+        {
+            id: 3,
+            text: 'You COOL, BROO!!',
+            src: 'https://sun9-55.userapi.com/s/v1/ig2/ZyBsxGr_o07pRdrAAniwTaTTNbXy4UIHSQffk5IdOGzeZV_Dr1byiJ3_m2zzdHKjnqNmRu53iphy4avSqZJiWJrM.jpg?quality=96&as=32x32,48x48,72x72,108x108,160x161,228x229&from=bu&cs=228x0'
+        },
+        {
+            id: 4,
+            text: 'Hi! How are you?',
+            src: 'https://sun9-55.userapi.com/s/v1/ig2/ZyBsxGr_o07pRdrAAniwTaTTNbXy4UIHSQffk5IdOGzeZV_Dr1byiJ3_m2zzdHKjnqNmRu53iphy4avSqZJiWJrM.jpg?quality=96&as=32x32,48x48,72x72,108x108,160x161,228x229&from=bu&cs=228x0'
+        },
+    ],
+    inputPostData: ''
+}
+
+const profileReducer = (state = initialState, action) => {
 
     switch (action.type) {
         case ADD_POST:
