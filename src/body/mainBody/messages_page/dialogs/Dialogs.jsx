a08b55380f7b8abda004b21aa592f06ed9c00e0c
Overbusher <kirill.raev.90@mail.ru>
2025-12-20 23:30:20 +0300

    Added DialogsContainer.js to the project. The data related to the 'store'(redux) has been moved from Dialogs.jsx to DialogsContainer.js to give Dialogs.jsx independence as a block.

--- src/body/mainBody/messages_page/dialogs/Dialogs.jsx
+++ src/body/mainBody/messages_page/dialogs/Dialogs.jsx
@@ -2,9 +2,8 @@ import {Dialog} from "./dialog/Dialog";
 import st from "./Dialogs.module.css"
 
 export const Dialogs = (props) => {
-    let state= props.store.getState();
 
-    let dialogData = state.messagesPage.dialogData.map(post =>
+    let dialogData = props.dialogData.map(post =>
         <Dialog key={post.id} id={post.id}
               name={post.name}/>)
 
