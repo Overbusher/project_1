d1a02b363e6dc72bf572b6ad6e1ea0d9b412cd47
Overbusher <kirill.raev.90@mail.ru>
2025-12-29 01:31:43 +0300

    The Users avatar source has been moved to the state.

--- src/body/mainBody/findUsers/FUsers/FUser/FUsers.jsx
+++ src/body/mainBody/findUsers/FUsers/FUser/FUsers.jsx
@@ -1,11 +1,11 @@
 import st from './FUsers.module.css'
 
 export const FUsers = (props) => {
-    debugger
     if (props.usersData.length === 0) {
         props.usersPush([
                 {
                     id: 1,
+                    avatar: 'https://cdn-icons-png.flaticon.com/512/6676/6676023.png',
                     fName: 'Diana',
                     lName: 'Raeva',
                     country: 'Belarus',
@@ -15,6 +15,7 @@ export const FUsers = (props) => {
                 },
                 {
                     id: 2,
+                    avatar: 'https://cdn-icons-png.flaticon.com/512/6676/6676023.png',
                     fName: 'Vitaliy',
                     lName: 'Raev',
                     country: 'Belarus',
@@ -24,6 +25,7 @@ export const FUsers = (props) => {
                 },
                 {
                     id: 3,
+                    avatar: 'https://cdn-icons-png.flaticon.com/512/6676/6676023.png',
                     fName: 'Olga',
                     lName: 'Raeva',
                     country: 'Belarus',
@@ -33,6 +35,7 @@ export const FUsers = (props) => {
                 },
                 {
                     id: 4,
+                    avatar: 'https://cdn-icons-png.flaticon.com/512/6676/6676023.png',
                     fName: 'Vera',
                     lName: 'Raeva',
                     country: 'Belarus',
@@ -42,6 +45,7 @@ export const FUsers = (props) => {
                 },
                 {
                     id: 5,
+                    avatar: 'https://cdn-icons-png.flaticon.com/512/6676/6676023.png',
                     fName: 'Timofei',
                     lName: 'Raev',
                     country: 'Belarus',
@@ -51,6 +55,7 @@ export const FUsers = (props) => {
                 },
                 {
                     id: 6,
+                    avatar: 'https://cdn-icons-png.flaticon.com/512/6676/6676023.png',
                     fName: 'Vanya',
                     lName: 'Batchkovich',
                     country: 'Ukrain',
@@ -67,7 +72,7 @@ export const FUsers = (props) => {
                     <div className={st.fUsers}>
                         <div className={st.avatar}>
                             <div className={st.avatar}><img
-                                src="https://cdn-icons-png.flaticon.com/512/6676/6676023.png"
+                                src={user.avatar}
                                 alt='Базовый аватар пользователя'/></div>
                             {user.followed ? <button onClick={() => {props.unfollow(user.id)}}>Отписаться</button> :
                                 <button onClick={() => {props.follow(user.id)}}>Подписаться</button>}
