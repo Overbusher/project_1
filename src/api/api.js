d8ebc191c809475247eb4ba7cdc2ed06dc0dee8a
Overbusher <kirill.raev.90@mail.ru>
2026-01-15 02:34:11 +0300

    Some code refactoring.

--- src/api/api.js
+++ src/api/api.js
@@ -1,35 +1,41 @@
 import axios from "axios";
 
+const instance = axios.create({
+    ithCredentials: true,
+    headers: {"API-KEY": "1b58b488-6bf1-4d5a-a89f-416bec40dd38"},
+    baseURL: "https://social-network.samuraijs.com/api/1.0/",
+})
+
 export const getUsers = (usersPageNumber, usersOnPageCount) => {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${usersPageNumber}&count=${usersOnPageCount}`, {
+    return instance.get(`users?page=${usersPageNumber}&count=${usersOnPageCount}`, {
         withCredentials: true,
         headers: {"API-KEY": "1b58b488-6bf1-4d5a-a89f-416bec40dd38"}
     }).then(response => response.data);
 }
 
 export const followPost = (id) => {
-    return axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${id}`, {}, {
+    return instance.post(`follow/${id}`, {}, {
         withCredentials: true,
         headers: {"API-KEY": "1b58b488-6bf1-4d5a-a89f-416bec40dd38"}
     }).then(response => response.data);
 }
 
 export const followDelete = (id) => {
-    return axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${id}`, {
+    return instance.delete(`follow/${id}`, {
         withCredentials: true,
         headers: {"API-KEY": "1b58b488-6bf1-4d5a-a89f-416bec40dd38"}
     }).then(response => response.data);
 }
 
 export const getAuth = () => {
-    return axios.get('https://social-network.samuraijs.com/api/1.0/auth/me', {
+    return instance.get('auth/me', {
         withCredentials: true,
         headers: {"API-KEY": "1b58b488-6bf1-4d5a-a89f-416bec40dd38"}
     }).then(response => response.data);
 }
 
 export const getProfile = (profileId) => {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${profileId}`, {
+    return instance.get(`profile/${profileId}`, {
         withCredentials: true,
         headers: {"API-KEY": "1b58b488-6bf1-4d5a-a89f-416bec40dd38"}
     }).then(response => response.data);
